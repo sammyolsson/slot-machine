7bc9b5b57e03f3e10f70a7ad4fe0dc6838cf0c20
@sammyolsson <sammyolsson@gmail.com>
2023-12-05 21:12:25 +0100

    Project start

---
+++ project.js
@@ -0,0 +1,20 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won/or not
+// 6. Give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+  const numberDepositAmount = parseFloat(depositAmount);
+
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log("Invalid deposit amount, try again.");
+  }
+};
+
+deposit();

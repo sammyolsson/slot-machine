2eb2c0ea554eef16587c2f718bb4337a13ceab29
@sammyolsson <sammyolsson@gmail.com>
2023-12-10 14:33:18 +0100

    Working on functions

--- project.js
+++ project.js
@@ -9,12 +9,30 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-  const depositAmount = prompt("Enter a deposit amount: ");
-  const numberDepositAmount = parseFloat(depositAmount);
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
 
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-    console.log("Invalid deposit amount, try again.");
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
   }
 };
 
-deposit();
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();

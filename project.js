3a000f4154fd91cc3b60c6791537a65fb02a3323
@sammyolsson <sammyolsson@gmail.com>
2023-12-11 20:10:38 +0100

    Symbols, spin

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOLS_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
@@ -46,6 +63,15 @@ const getBet = (balance, lines) => {
     }
   }
 };
+
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    console.log(symbol, count);
+  }
+};
+
+spin();
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();

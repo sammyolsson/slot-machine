79e763c78719b9247ce03651aaeedcabf76d8d89
@sammyolsson <sammyolsson@gmail.com>
2023-12-29 20:32:53 +0100

    transpose

--- project.js
+++ project.js
@@ -72,8 +72,9 @@ const spin = () => {
     }
   }
 
-  const reels = [[], [], []];
+  const reels = [];
   for (let i = 0; i < COLS; i++) {
+    reels.push([]);
     const reelSymbols = [...symbols];
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -86,7 +87,16 @@ const spin = () => {
   return reels;
 };
 
-spin();
+const transpose = (reels) => {
+  const rows = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    rows.push({});
+    for (let j = 0; j < COLS; j++) {}
+  }
+};
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
